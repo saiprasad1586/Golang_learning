7133a6f914a4789536fae2a9aa1cdd64e8352f15
saiprasadgovindu <saiprasadgovindu@gmail.com>
2020-02-06 19:56:22 +0530

    Initial commit

---
+++ basics/hello.go
@@ -0,0 +1,6 @@
+// package main
+
+// func main() {
+//  var score int = 4
+// 	fmt.Println("score")
+// }
---
+++ basics/var.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "I am saiprasad"
+	fmt.Println(a)
+
+	var b string
+	b = "I am from Gitam"
+	fmt.Println(b)
+
+	c := "I am a Programmer"
+	fmt.Println(c)
+
+	c_rating := 3.
+	// %v -> value formating option
+	// %T from type of data the variable is storing
+	// 3.0 and 3. are Float variables
+	fmt.Printf("%v, %T \n", c_rating, c_rating)
+
+	var d, e int = 10, 20
+	// just like python decelaration
+	fmt.Println(d, e)
+
+	// Weird type in Go
+
+	var (
+		saiprasad = "I am saiprasad"
+		age       = 18
+		power     = "Programming"
+	)
+	fmt.Println(saiprasad, age, power)
+}
